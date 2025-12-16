6fa18d2c6f34cf4cba8410827eea23f149b510e7
talia <LILTULz@gmail.com>
2025-12-16 17:04:55 +0400

    thitd commit

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,6 +4,6 @@ public class Test {
         System.out.println("hello world");
         System.out.println("this is the project version 2");
         System.out.println("hello world ");
-
+        System.out.println(" hey tt");
     }
 }
