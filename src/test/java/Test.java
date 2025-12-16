b82882139f8c172ab1ca6eb43ff138838d2163b3
talia <LILTULz@gmail.com>
2025-12-16 16:59:39 +0400

    second commit

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,7 +2,8 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("hello world");
-        System.out.println("this is the project version 1");
-      
+        System.out.println("this is the project version 2");
+        System.out.println("hello world ");
+
     }
 }
